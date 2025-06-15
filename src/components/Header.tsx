35d8cdfffe24f1944adf0ac55df8d42621d52b28
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2025-06-15 15:46:04 -0400

    change nav sections to link, add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,14 @@ function Header() {
     return (
         <div className='header-main'>
             <nav className='navbar'>
-                <div className='navbar-section'>Z-Score App</div>
-                <div className='navbar-section'>Games</div>
-                <div className='navbar-section'>Publications</div>
-                <div className='navbar-section'>Scores</div>
+                <div className='navbar-section'>
+                    {/* link below is a placeholder until logo img finalized */}
+                    <img id='logo' src='src/assets/z-score-logo.png' alt='logo'/>
+                </div>
+                <a className='navbar-section'>Z-Score App</a>
+                <a className='navbar-section'>Games</a>
+                <a className='navbar-section'>Publications</a>
+                <a className='navbar-section'>Scores</a>
             </nav>
         </div>
     )
