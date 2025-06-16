32c74bf2434cf46a2ae0851367427b5041bcf14a
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2025-06-15 21:29:14 -0400

    get router working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
 import './App.css'
 import Header from './components/Header'
 import Footer from "./components/Footer.tsx";
+import HomePage from "./components/page/HomePage.tsx";
+import PublicationListPage from "./components/page/PublicationListPage.tsx";
+import GameListPage from "./components/page/GameListPage.tsx";
 
 function App() {
 
@@ -8,6 +16,15 @@ function App() {
         <>
             <Header/>
             {/* The generic page body would go here... the "outlet" in react */}
+
+            <Router>
+                <Routes>
+                    <Route path="/" element={ <HomePage /> }/>
+                    <Route path="/publication" element={ <PublicationListPage /> }/>
+                    <Route path="/game" element={ <GameListPage /> }/>
+                </Routes>
+            </Router>
+
             <Footer/>
         </>
     );
